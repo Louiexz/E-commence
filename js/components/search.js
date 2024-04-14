db475edd179da9f519ab44da3eaf7de2d9a0622d
Louiexz <fluizlucas@gmail.com>
2024-04-14 13:32:22 -0300

    Adicionando imagens e refatorando js

--- js/components/search.js
+++ js/components/search.js
@@ -4,7 +4,7 @@ export class SearchEngine {
     }
 
     searchProduct() {
-        fetch("./json/products.json")
+        fetch("../../json/products.json")
             .then(response => response.json())
             .then(data => {
                 if (data && data.Electronics && data.Electronics.length > 0) {
@@ -24,11 +24,11 @@ export class SearchEngine {
     
                     // Verifica se algum produto foi encontrado
                     if (foundProducts.length > 0) {
-                        // Converter a lista de produtos encontrados em uma string JSON para passar como parâmetro na URL
-                        let productsString = JSON.stringify(foundProducts);
+                        // Salvar os produtos encontrados no localStorage
+                        localStorage.setItem('foundProducts', JSON.stringify(foundProducts));
     
-                        // Navegar para a outra página passando a lista de produtos encontrados como um parâmetro
-                        window.location.href = '../../produtos.html?products=' + encodeURIComponent(productsString);
+                        // Navegar para a outra página de produtos
+                        window.location.href = '../../produtos.html';
     
                     } else {
                         window.alert('Sem produtos desse modelo!');
@@ -44,35 +44,30 @@ export class SearchEngine {
     
     // Função para exibir os produtos encontrados na página de produtos
     resultProducts() {
-        let productsString = decodeURIComponent(window.location.search.split('=')[1]);
+        // Recuperar os produtos encontrados do localStorage
+        let productsString = localStorage.getItem('foundProducts');
 
-        // Converter a string de produtos de volta para uma lista de objetos JavaScript
-        let products = JSON.parse(productsString);
-        
-        const productGrid = document.getElementById('product_grid');
-        productGrid.innerHTML = '';
-        
-        if (products.length > 0) {
-            products.forEach(product => {
-                const productElement = document.createElement('div');
-                productElement.classList.add('product_box');
-                
-                let message = `<h2>${product.name}</h2><p>$ ${product.price}</p><p>${product.description}</p>`;
-                productElement.innerHTML = message;
-                
-                productGrid.appendChild(productElement);
-            });
+        if (productsString) {
+            let products = JSON.parse(productsString);
+            const productGrid = document.getElementById('product_grid');
+            productGrid.innerHTML = '';
+            
+            if (products.length > 0) {
+                products.forEach(product => {
+                    const productElement = document.createElement('div');
+                    productElement.classList.add('product_box');
+                    
+                    console.log(product.image)
+                    let message = `<h2>${product.name}</h2><img src="${product.image}.jpg"><p>R$ ${product.price}</p><p>${product.description}</p>`;
+                    productElement.innerHTML = message;
+                    
+                    productGrid.appendChild(productElement);
+                });
+            } else {
+                productGrid.innerHTML = '<p>Nenhum produto encontrado.</p>';
+            }
         } else {
-            productGrid.innerHTML = '<p>Nenhum produto encontrado.</p>';
-        }
-    }
-
-    showProduct(products) {
-        const productBox = document.getElementById('product_box');
-        if (products.length > 0) {
-            productBox.innerHTML = `<h1>${products[0].name}</h1><p>${products[0].description}</p><p>${products[0].price}</p>`;
-        } else {
-            productBox.innerHTML = '<p>Nenhum produto encontrado.</p>';
+            window.alert('Nenhum produto encontrado.');
         }
     }
 }
--- js/index.js
+++ js/index.js
@@ -4,31 +4,24 @@ import { SearchEngine } from './components/search.js';
 const login = new LoginController();
 const search = new SearchEngine();
 
-// Login
+// Permite visualizar a area de login
 document.getElementById('toggle_login').addEventListener('click', () => {
   login.toggle_login();
 });
+// Verifica as credenciais da area de login
 document.getElementById('user_login').addEventListener('click', () => {
   login.check_login();
 });
-
 // Pesquisar produtos
 document.getElementById('search').addEventListener('keyup', function(event) {
   if (event.key === 'Enter') {
     search.searchProduct();
   }
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-  var produtosSection = document.getElementById('products');
-  if (produtosSection) {
-      console.log('Elemento com ID "produtos" encontrado.');
-      search.resultProducts();
-  } else {
-      console.error('Elemento com ID "produtos" não encontrado.');
-  }
-});
-
+// Verifica se está na página produtos
+if (window.location.pathname.includes('produtos.html')) {
+  search.resultProducts();
+}
 /*
 import { RegisterController } from './pages/register.js';
 
